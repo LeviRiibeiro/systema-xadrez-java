3c025e11311a5aaf02aa474c6d4d8546cadc0785
LeviRiibeiro <levi.ribeiro2345@gmail.com>
2023-06-11 22:25:48 -0300

    Primeira classe: Posicao

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Posicao pos = new Posicao (3, 5);
+        System.out.println(pos);
+
+    }
+}
