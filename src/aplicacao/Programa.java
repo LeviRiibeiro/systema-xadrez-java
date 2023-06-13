6490974691384076e9388d18318480a372b9a67a
LeviRiibeiro <levi.ribeiro2345@gmail.com>
2023-06-12 21:00:48 -0300

    Iniciando a implementação do Tabuleiro e peças

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,12 +1,12 @@
 package aplicacao;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Programa {
     public static void main(String[] args) {
 
-        Posicao pos = new Posicao (3, 5);
-        System.out.println(pos);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
     }
 }
